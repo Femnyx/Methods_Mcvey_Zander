5efb965697c1f038f881f3c860e660c139fc3768
Zander <114107976+Femnyx@users.noreply.github.com>
2023-04-10 14:31:11 -0500

    Hey arthur!

--- Methods_Mcvey_Zander/Program.cs
+++ Methods_Mcvey_Zander/Program.cs
@@ -6,7 +6,43 @@ namespace Methods_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a whole number!");
+           
+            //x is a placeholder for a number.
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another whole number!");
+            
+            //y is a placeholder for a number.
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            //Multiplies x (num1) and y (num2).
+            Console.WriteLine(Multiply(x,y));
+
+            //The Console asks the user for their name.
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            WelcomeUser(name);
+
+            Goodbye();
+        }
+
+        static int Multiply(int num1, int num2)
+        {
+            //Multiplies num 1 and num2.
+            return num1 * num2;
+        }
+
+        static void WelcomeUser(string username)
+        {
+            //The Console greets the user.
+            Console.WriteLine($"Hello, {username}! It is nice to meet you!");
+        }
+
+        static void Goodbye()
+        {
+            //The Console tells the user to have a fantastic day.
+            Console.WriteLine("Have a fantastic day!");
         }
     }
 }
